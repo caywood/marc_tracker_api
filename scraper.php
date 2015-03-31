55295483f30870e8d39818875c4e17ae6c831e84
Matt Caywood <caywood@users.noreply.github.com>
2015-03-31 17:26:34 -0400

    Automatic update to make ScraperWiki scraper work on Morph

--- scraper.php
+++ scraper.php
@@ -1,4 +1,5 @@
 <?php
+require 'scraperwiki.php';
 
 require 'scraperwiki/simple_html_dom.php';           
 $html_content = scraperwiki::scrape("http://www.marctracker.com/PublicView/location.jsp");
